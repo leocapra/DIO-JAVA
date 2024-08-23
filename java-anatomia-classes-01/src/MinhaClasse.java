78f0112d73b12e58b09fa2bee85663489046e5b6
Leonardo <leoccapra@gmail.com>
2024-08-23 14:39:31 -0300

    feat: exemplo de tipos de variaveis

--- java-anatomia-classes-01/src/MinhaClasse.java
+++ java-anatomia-classes-01/src/MinhaClasse.java
@@ -1,9 +1,15 @@
 public class MinhaClasse {
 
 public static void main(String[] args) {
+    String primeiroNome = "Gleyson";
+    String segundoNome = "Sampaio";
 
-    System.out.print ("Hello world");
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
 
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
 }
 
 }
