376533cdf17bdcd1a34f0486dd4650dff29a7289
Leonardo <leoccapra@gmail.com>
2024-08-23 14:51:06 -0300

    add exemplo de identação

---
+++ java-anatomia-classes-02/src/BoletimEstudantil.java
@@ -0,0 +1,23 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+//exemplos de identação (sem identação)
+    //int mediaFinal = 6;
+    //if(mediaFinal<6)
+    //System.out.println("REPROVADO");
+    //else if(mediaFinal==6)
+    //System.out.println("PROVA MINERVA");
+    //else
+    //System.out.println("PROVADO");
+
+
+//exemplos de identação (com identação)
+
+        int mediaFinal = 6;
+            if(mediaFinal<6)
+                System.out.println("REPROVADO");
+            else if(mediaFinal==6)
+                System.out.println("PROVA MINERVA");
+            else
+                System.out.println("PROVADO");
+    }
+}
